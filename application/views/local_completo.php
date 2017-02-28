29da73d541bb73b2b5a55905edafbdca2959ea79
Marcio Gimenez <marciogimenez1093@gmail.com>
2017-02-28 11:03:16 -0300

    coordenadas

--- application/views/local_completo.php
+++ application/views/local_completo.php
@@ -98,7 +98,7 @@
 	function initMap() {
         // Create a map object and specify the DOM element for display.
         var map = new google.maps.Map(document.getElementById('map'), {
-        	center: {lat: -34.397, lng: 150.644},
+        	center: {lat: <?=$local[0]->latitude?>, lng: <?=$local[0]->longitude?>},
         	scrollwheel: false,
         	zoom: 8
         });
